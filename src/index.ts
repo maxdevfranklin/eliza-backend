59f251e52334297865ec11dd3b68bab83c3ad3ba
maxdevfranklin <max.franklin.tech@gmail.com>
2025-06-06 04:03:04 -0500

    change action extension

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ import {
   parseArguments,
 } from "./config/index.ts";
 import { initializeDatabase } from "./database/index.ts";
-import { grandVillaAction } from "./actions/grand-villa";
+import { grandVillaAction } from "./actions/grand-villa.ts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
